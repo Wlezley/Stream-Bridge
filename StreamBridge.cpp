5e87167519db5a4a5821067f758868d64a75848d
Wlezley Lishack <wlezley.lishack@gmail.com>
2026-04-18 20:35:06 +0200

    feat: REST API testing EP
    TODO: Implement OpenSSL

--- StreamBridge.cpp
+++ StreamBridge.cpp
@@ -10,6 +10,7 @@
 #include <string>
 #include <vector>
 #include <windows.h>
+#include <sstream>
 #include <iostream>
 
 #include <Poco/Thread.h>
@@ -21,22 +22,67 @@ using namespace std;
 #include "src/utils/ExtendedStd.hpp"
 //#include "src/connectors/"
 
+//TEST EP:
+//https://api.restful-api.dev/collections
+
+#include <Poco/Net/HTTPClientSession.h>
+#include <Poco/Net/HTTPRequest.h>
+#include <Poco/Net/HTTPResponse.h>
+#include <Poco/StreamCopier.h>
+
+// TEST...
 int main()
 {
-    EventBus bus;
+    try {
+        Poco::Net::HTTPClientSession session("api.restful-api.dev", 80);
+        session.setKeepAlive(true);
 
-    Poco::Thread cliThread;
-    Poco::Thread inputThread;
+        // request
+        Poco::Net::HTTPRequest request(
+            Poco::Net::HTTPRequest::HTTP_GET,
+            "/objects",
+            Poco::Net::HTTPMessage::HTTP_1_1
+        );
 
-    //cliThread.startFunc(inputLoop);
-    //inputThread.startFunc([&bus]() { inputLoop(bus); });
+        session.sendRequest(request);
 
-    Logger::log("App started");
+        // response
+        Poco::Net::HTTPResponse response;
+        std::istream& rs = session.receiveResponse(response);
 
-    std::cin.get();
+        std::stringstream ss;
+        Poco::StreamCopier::copyStream(rs, ss);
+
+        std::cout << "Status: " << response.getStatus() << std::endl;
+        std::cout << "Body:\n" << ss.str() << std::endl;
+    }
+    catch (const std::exception& e) {
+        std::cerr << "ERROR: " << e.what() << std::endl;
+    }
 
     return 0;
 }
+
+
+
+
+
+//int main()
+//{
+//    EventBus bus;
+//
+//    Poco::Thread cliThread;
+//    Poco::Thread inputThread;
+//
+//    //cliThread.startFunc(inputLoop);
+//    //inputThread.startFunc([&bus]() { inputLoop(bus); });
+//
+//    Logger::log("App started");
+//
+//    std::cin.get();
+//
+//    return 0;
+//}
 
 void inputLoop(EventBus& bus)
 {
